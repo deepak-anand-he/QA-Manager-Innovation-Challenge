e1e2b20e297e9b016e0aae1d8ddc56020f3485ac
Deepak Anand Mani <deepak-anand.mani@hitachienergy.com>
2025-05-30 13:57:31 +0530

    git ignore updated

---
+++ src/config/jira.properties.js
@@ -0,0 +1,4 @@
+export const jiraConfig = {
+//   username: 'deepak-anand.mani@hitachienergy.com',
+  //apiToken: 'ATATT3xFfGF0lyPJ7Kz7eNZCVyi7objaeIuKesrR46yMN1SQH_lxJKh0JRXhboTvkd_Vhjql28TlizkTcXKieVTf5kYu9O3ayN5hbKSOoVu6dgBp4EGA9TSpVjJQcyK7kkknurjn8nV8yqJ0iseZ_wfTM1cbRQgfGpJBL6ufZstFpcJ_hohvR_k=8C4F7AEE',
+};
